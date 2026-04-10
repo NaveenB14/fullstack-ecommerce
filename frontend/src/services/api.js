e70bfefb8a3d78efd809a1e5b2836c1c09b98bf5
Naveen B <147124405+NaveenB14@users.noreply.github.com>
2026-04-10 19:15:20 +0530

    Change API base URL to production endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://fullstack-ecommerce-mz3v.onrender.com"
 });
 
 export default API;
